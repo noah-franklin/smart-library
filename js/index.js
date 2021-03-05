851a845c19686aaa21d6d53b141e6e7fcd09a4fe
Noah Franklin <noah6198@aol.com>
2021-03-05 12:17:41 -0500

    fixed links again

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // jQuery methods go here...
-  $(".navybar").load("/html/navbar.html")
-  $(".main").load("/html/home.html");
-  $(".footer").load("/html/footer.html");
+  $(".navybar").load("../html/navbar.html")
+  $(".main").load("../html/home.html");
+  $(".footer").load("../html/footer.html");
 });
