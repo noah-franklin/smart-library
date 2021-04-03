eaac6b4371b9cbc65c4b2cc375d2d17f7bea1082
Noah Franklin <noah6198@aol.com>
2021-04-03 14:33:46 -0400

    further working on script and styled bookshelf

--- testing/shelfgenerator.py
+++ testing/shelfgenerator.py
@@ -2,8 +2,12 @@ import os
 
 directory = './'
 
+
 def get_stuff(directory, naming):
-    for file in os.listdir(directory):
+    files = os.listdir(directory)
+    files.sort()
+    
+    for file in files:
         if file.startswith(".") or file.endswith(".py") or file.startswith("bookcases"):
             continue
         else:
@@ -12,7 +16,27 @@ def get_stuff(directory, naming):
                 continue
             else:
                 if (file.startswith("c")):
-                    f = open("./bookcases/"+file+".html", "w")
+                    global parent
+                    global shelf
+                    shelf = file
+                    parent = "./bookcases/"+file
+                    os.mkdir(parent)
+
+                if (file.startswith("a")):
+                    global f
+                    f = open(parent +"/a.html" , "w")
+                    f.write("Shelf: "+shelf+"\nSide: a\n")
+
+                if (file.startswith("b")):
+                    global f
+                    f = open(parent +"/b.html" , "w")
+                    f.write("Shelf: "+shelf+"Side: b\n")
+
+                if(file.startswith("s")):
+                    f.write(file+"\n")
+                if(file.startswith("r")):
+                    f.write(file+"\n")
+                    
                 get_stuff(directory+file+"/", "Sub Folders")
 
 
