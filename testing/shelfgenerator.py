3eb62f4c2127245ca2f2333f7dd8c59e861665cd
Noah Franklin <noah6198@aol.com>
2021-04-03 13:33:25 -0400

    finished rough draft of bookcase page
    Co-authored-by: Yitzhak Alvarez <yitzhak.alvarez1998@yahoo.com>
    Co-authored-by: alex-m14 <alex-m14@users.noreply.github.com>

--- testing/shelfgenerator.py
+++ testing/shelfgenerator.py
@@ -4,15 +4,16 @@ directory = './'
 
 def get_stuff(directory, naming):
     for file in os.listdir(directory):
-        if file.startswith(".") or file.endswith(".py"):
+        if file.startswith(".") or file.endswith(".py") or file.startswith("bookcases"):
             continue
         else:
             print("\n" + naming + ": " + file)
             if file.endswith(".jpg"):
-                
                 continue
             else:
+                if (file.startswith("c")):
+                    f = open("./bookcases/"+file+".html", "w")
                 get_stuff(directory+file+"/", "Sub Folders")
 
 
-get_stuff(directory, "#-NAME")
+get_stuff(directory, "#-Parent")
