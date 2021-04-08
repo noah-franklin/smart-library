18382165a6ba64f0852d763e3006f44e7e089579
Noah Franklin <noah6198@aol.com>
2021-04-07 21:53:10 -0400

    finished version 1 of shelf generator scripts, created html bookcase skeleton page
    Co-authored-by: Yitzhak Alvarez <yitzhak.alvarez1998@yahoo.com>
    Co-authored-by: alex-m14 <alex-m14@users.noreply.github.com>

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,4 @@
 function tester() {
-  $(".main").load("./html/bookShelf.html");
+  // $(".main").load("./html/bookShelf.html");
+  $(".main").load("./testing/bookcases/c01/a.html");
 }
--- testing/shelfgenerator.py
+++ testing/shelfgenerator.py
@@ -6,38 +6,69 @@ directory = './'
 def get_stuff(directory, naming):
     files = os.listdir(directory)
     files.sort()
-    
+    print(files)
     for file in files:
         if file.startswith(".") or file.endswith(".py") or file.startswith("bookcases"):
             continue
         else:
             print("\n" + naming + ": " + file)
+            if file.endswith(".jpg") and naming.startswith("Photos"):
+               
+                f.write('<img class="bookImg" src="http://a.cs.newpaltz.edu/vlib/idatabase/'+directory[2:]+file+'"'+' />')
+            
             if file.endswith(".jpg"):
                 continue
-            else:
-                if (file.startswith("c")):
-                    global parent
-                    global shelf
-                    shelf = file
-                    parent = "./bookcases/"+file
-                    os.mkdir(parent)
+            if (file.startswith("c")):
+                global parent
+                global shelf
+                shelf = file
+                parent = "./bookcases/"+file
+                os.mkdir(parent)
+                get_stuff(directory+file+"/", "A or B")
 
-                if (file.startswith("a")):
-                    global f
-                    f = open(parent +"/a.html" , "w")
-                    f.write("Shelf: "+shelf+"\nSide: a\n")
+            if (file.startswith("a")):
+                global f
+                f = open(parent +"/a.html" , "w")
+                f.write('<head>\n')
+                f.write('<link href="./css/bookShelf.css" rel="stylesheet" />\n')
+                f.write('</head>\n')
+                f.write("<h1>Shelf: "+shelf+"\nSide: a</h1>\n")
+                f.write('<div class="container-fluid shelf">\n')
+                f.write('<div class="row NOWRAPPLS">\n')
+                get_stuff(directory+file+"/", "Sections")
+                f.write("</div>\n")
+                f.write("</div>\n")
 
-                if (file.startswith("b")):
-                    global f
-                    f = open(parent +"/b.html" , "w")
-                    f.write("Shelf: "+shelf+"Side: b\n")
+            if (file.startswith("b")):
+                global f
+                f = open(parent +"/b.html" , "w")
+                f.write('<head>\n')
+                f.write('<link href="./css/bookShelf.css" rel="stylesheet" />\n')
+                f.write('</head>\n')
+                f.write("<h1>Shelf: "+shelf+"\nSide: b</h1>\n")
+                f.write('<div class="container-fluid shelf">\n')
+                f.write('<div class="row NOWRAPPLS">\n')
+                get_stuff(directory+file+"/", "Sections")
+                f.write("</div>\n")
+                f.write("</div>\n")
 
-                if(file.startswith("s")):
-                    f.write(file+"\n")
-                if(file.startswith("r")):
-                    f.write(file+"\n")
+            if(file.startswith("s")):
+                    # f.write(file+"\n")
+                f.write('<div class="col section">\n')
+                get_stuff(directory+file+"/", "Rows")
+                f.write("</div>\n")
+            if(file.startswith("r")):
+                    # f.write(file+"\n")
+                f.write('<div class="rowStyle">\n')
+                get_stuff(directory+file+"/", "Photos")
+                f.write("</div>\n")
+            if(file[0].isdigit()):
+                global owner
+                owner = file
+                get_stuff(directory+file+"/", "Bookshelves")
                     
-                get_stuff(directory+file+"/", "Sub Folders")
+            # get_stuff(directory+file+"/", "Sub Folders")
+                
 
 
 get_stuff(directory, "#-Parent")
