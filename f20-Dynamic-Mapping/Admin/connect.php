c6908ef708810a090169d64935bd123a9bae52c4
Noah Franklin <noah6198@aol.com>
2021-04-10 13:48:52 -0400

    working more on display the bookcases, working on bookshelf html

--- f20-Dynamic-Mapping/Admin/connect.php
+++ f20-Dynamic-Mapping/Admin/connect.php
@@ -1,8 +1,14 @@
 <?php
+    // $servername = "localhost";
+    // $username = "p_f17_7";
+    // $password = "191fne";
+    // $database = "p_f17_7_db";
+
     $servername = "localhost";
     $username = "p_s21_1";
     $password = "dkxqt5";
     $database = "p_s21_1_db";
+
     
 
     $conn = new mysqli($servername, $username, $password, $database);
