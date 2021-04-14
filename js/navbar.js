0d6b2cbc8711b7e1dc5e47ccf4cce0a86530fb5d
Noah Franklin <noah6198@aol.com>
2021-04-14 11:48:45 -0400

    Bookcase and Bookshelf pages Finished the skeleton of bookshelf.html page and added it to the python script. Ran it on the "a" server and transfer the html to this repo.

--- js/navbar.js
+++ js/navbar.js
@@ -4,9 +4,29 @@ function tester() {
 }
 
 function tester1() {
-  $(".main").load("./html/bookcases/c01/a.html");
+  $(".main").load("./html/bookCase.html");
+  //$(".main").load("./html/bookcases/c01/a.html");
 }
 
-function getBookCase(caseNum) {
-  $(".main").load("./html/bookcases/" + caseNum + "/a.html");
+function getBookCase(caseNum, side) {
+  $(".main").load(
+    "./html/bookcases/" + caseNum + "/" + side + ".html",
+    function () {
+      if (side == "a") {
+        $(".aSide").css("background-color", "orange").css("color", "white");
+        $(".bSide").css("background-color", "white").css("color", "black");
+        console.log("a");
+      }
+      if (side == "b") {
+        $(".bSide").css("background-color", "orange").css("color", "white");
+        $(".aSide").css("background-color", "white").css("color", "black");
+        console.log("b");
+      }
+    }
+  );
+
+}
+
+function getBookShelf(dir, row) {
+  $(".main").load("./html" + "/" + dir + "/" + row + ".html");
 }
